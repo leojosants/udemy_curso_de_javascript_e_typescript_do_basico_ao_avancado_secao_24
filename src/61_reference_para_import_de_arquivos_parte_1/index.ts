5258516b111f23007928e47f4d1b9d7c157afdfe
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-20 20:19:03 -0300

    335. Creating a namespace

---
+++ src/61_reference_para_import_de_arquivos_parte_1/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="modulo/module.ts" />
+
+import { MyNamespace } from "./modulo/module";
+
+console.log(MyNamespace.AnotherNamespace);
+
+/*
+  $ npx tsc src/61_reference_para_import_de_arquivos_parte_1/index.ts --outFile src/61_reference_para_import_de_arquivos_parte_1/index.js
+  - erro -> Cannot compile modules using option 'outFile' unless the '--module' flag is 'amd' or 'system'.
+  - nao funcionou nem alterado o sistema de modulos para AMD nem para System
+  somenter usando importa e executando pelo codeRunner
+ */
---
+++ src/61_reference_para_import_de_arquivos_parte_1/modulo/module.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/*- - - - - - - - - - - - - - - - namespaces - - - - - - - - - - - - - - - -*/
+export namespace MyNamespace {
+  /*- - - - - - - - - - - - - - - - classes - - - - - - - - - - - - - - - -*/
+  export class PersonNamespace {
+    public name: string;
+
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+
+  export const nameNamespace = 'Nome 1';
+
+  const personNamespace = new PersonNamespace('Nome 2');
+  console.log(personNamespace);
+
+  /*- - - - - - - - - - - - - - - - namespaces - - - - - - - - - - - - - - - -*/
+  export namespace AnotherNamespace {
+    export const nameAnotherNamespace = 'Nome 3';
+
+  }
+}
+
+/*- - - - - - - - - - - - - - - - functions - - - - - - - - - - - - - - - -*/
+export function runMain(): void {
+  const personNamespace = new MyNamespace.PersonNamespace('Nome 4');
+  console.log(personNamespace);
+  console.log(MyNamespace.nameNamespace);
+  console.log(MyNamespace.AnotherNamespace.nameAnotherNamespace);
+}
+
+/*- - - - - - - - - - - - - - - - main - - - - - - - - - - - - - - - -*/
+runMain();
