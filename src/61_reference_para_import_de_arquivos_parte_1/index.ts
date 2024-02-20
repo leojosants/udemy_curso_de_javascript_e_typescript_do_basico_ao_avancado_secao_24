3f39cb8a2222ffa4d401669c1f726e07b0c8b055
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-20 20:20:45 -0300

    336. File Import Reference - Part 1

--- src/61_reference_para_import_de_arquivos_parte_1/index.ts
+++ src/61_reference_para_import_de_arquivos_parte_1/index.ts
@@ -7,7 +7,7 @@ console.log(MyNamespace.AnotherNamespace);
 
 /*
   $ npx tsc src/61_reference_para_import_de_arquivos_parte_1/index.ts --outFile src/61_reference_para_import_de_arquivos_parte_1/index.js
-  - erro -> Cannot compile modules using option 'outFile' unless the '--module' flag is 'amd' or 'system'.
-  - nao funcionou nem alterado o sistema de modulos para AMD nem para System
-  somenter usando importa e executando pelo codeRunner
+    - erro -> Cannot compile modules using option 'outFile' unless the '--module' flag is 'amd' or 'system'.
+    - nao funcionou nem alterado o sistema de modulos para AMD nem para System
+    - somenter usando importa e executando pelo codeRunner
  */
